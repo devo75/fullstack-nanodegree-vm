ca95a65066e04f8c84dd7790cbbe8fd7f027746b
unknown <dcocar@hotmail.com>
2019-05-12 12:06:03 +0800

    Fix code to add and print MenuItem

--- vagrant/crud/restaurant_app.py
+++ vagrant/crud/restaurant_app.py
@@ -9,11 +9,20 @@ session = DBSession()
 
 myFirstRestaurant = Restaurant(name = "Pizza Palace")
 session.add(myFirstRestaurant)
-sesssion.commit()
+session.commit()
+
+myFirstRestaurant = Restaurant(name = "Pizza Palace")
+session.add(myFirstRestaurant)
+session.commit()
+
+cheesepizza = MenuItem(name="Cheese Pizza", description = "Made with all natural ingredients and fresh mozzarella", course="Entree", price="$8.99", restaurant=myFirstRestaurant)
+session.add(cheesepizza)
+session.commit()
 
 firstResult = session.query(Restaurant).first()
 firstResult.name
 
 items = session.query(MenuItem).all()
 for item in items:
-    print item.name
+    print(item.name)
+
