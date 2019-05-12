c38533bc89d4150c6d766690059e61cdf6bd704a
unknown <dcocar@hotmail.com>
2019-05-12 12:10:28 +0800

    Format menuitem

--- vagrant/crud/restaurant_app.py
+++ vagrant/crud/restaurant_app.py
@@ -15,7 +15,11 @@ myFirstRestaurant = Restaurant(name = "Pizza Palace")
 session.add(myFirstRestaurant)
 session.commit()
 
-cheesepizza = MenuItem(name="Cheese Pizza", description = "Made with all natural ingredients and fresh mozzarella", course="Entree", price="$8.99", restaurant=myFirstRestaurant)
+cheesepizza = MenuItem(name="Cheese Pizza", 
+                       description = "Made with all natural ingredients and fresh mozzarella", 
+                       course="Entree", 
+                       price="$8.99", 
+                       restaurant=myFirstRestaurant)
 session.add(cheesepizza)
 session.commit()
 
